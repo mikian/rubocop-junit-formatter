78b151fd1f78007ec7173d3bc1527ad106580a91
Yong Wen Chua <chua_yong_wen@ida.gov.sg>
2016-02-04 12:58:17 +0800

    Emit all test cases for passing files

--- lib/rubocop/formatter/junit_formatter.rb
+++ lib/rubocop/formatter/junit_formatter.rb
@@ -19,8 +19,6 @@ module RuboCop
       end
 
       def file_finished(file, offences)
-        return if offences.empty?
-
         # One test case per cop per file
         COPS.each do |cop|
           REXML::Element.new('testcase', @testsuite).tap do |f|
